6a4c43dd6bc42d9a68e5893dab25933b52c3dffc
Joe Hoang <josephhoang97@gmail.com>
2019-03-07 02:42:59 +0000

    added pages

---
+++ src/pages/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './App.css';
+
+import HomePage from './HomePage/HomePage';
+import AboutPage from './HomePage/HomePage';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/topics">Topics</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={HomePage} />
+          <Route path="/about" component={AboutPage} />
+          
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
